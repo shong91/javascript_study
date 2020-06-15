ba1ddc19a979188cc158a00a504309c892561626
Soyeon Hong <52073383+hhhongso@users.noreply.github.com>
2020-06-15 23:08:35 +0900

    Create destructuring.js

---
+++ ch03/destructuring.js
@@ -0,0 +1,32 @@
+// 구조 분해 할당 Destructuring
+
+let data = ["crong". "honux", "jk", "jinny"];
+
+// 배열의 특정 인덱스 값을 변수로 사용하기
+let myName = data[0];
+let jisu = data[1];
+let jung = data[2];
+
+// destructing 으로 배열/오브젝트에 원하는 값 할당하기
+let [jisu, ,jung] = data; // "crong", "jk";
+
+
+
+let obj = {
+    name: "crong",
+    address : "korea",
+    age: 10
+};
+
+let {name, age} = obj;
+
+let {name: myName, age: myAge} = obj;
+console.log(myName, myAge); //"crong" , 10;
+
+
+// 새로운 변수 이름으로 할당하기
+var o = {p: 30, q:true};
+var {p: foo, q: bar} = o;
+
+console.log(foo); // 30; 
+console.log(bar); // true
