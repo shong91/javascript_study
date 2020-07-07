8b18607b09a057cc5bbe3a9c133703ea7cfd5b69
Soyeon Hong <52073383+hhhongso@users.noreply.github.com>
2020-07-07 23:27:14 +0900

    babel preset setting

--- miniProject/index.js
+++ miniProject/index.js
@@ -1,1 +1,7 @@
 console.log('start project by webpack!');
+
+
+const myfunction = () => {
+    let name = "hong";
+    console.log(`my name is ${name}`);
+}
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,20 @@ module.exports = {
     module: {
         rules: [{
             // /\.css
+            test: /\.js$/,
+            include: path.resolve(__dirname, 'src'),
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env', {
+                        'targets' : {
+                            'browsers': ["last 2 versions"]
+                             
+                        },
+                        "debug": true
+                    }]
+                }
+            }
         }]
     }
 }
