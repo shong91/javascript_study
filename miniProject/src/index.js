251911c5810378ae78c9a82eded0be086d89c910
Soyeon Hong <52073383+hhhongso@users.noreply.github.com>
2020-07-13 21:51:45 +0900

    miniProject - ajax load

--- miniProject/src/index.js
+++ miniProject/src/index.js
@@ -1,7 +1,6 @@
 import blog from "./main.js";
 
 const myblog = new blog();
-
 const myfunction = () => {
     let name = "hong";
     console.log(`my name is ${name}`);
--- miniProject/src/main.js
+++ miniProject/src/main.js
@@ -1,8 +1,38 @@
 class Blog {
     constructor(){
-        console.log('Blog is started!')
+        console.log('Blog is started!');
+        const dataURL = "./data/data.json";
+        this.setInitData(dataURL);
     }
+
+    setInitData(dataURL) {
+        this.getData(dataURL);
+    }
+
+    getData(dataURL){
+        //ajax 호출
+        const oReq = new XMLHttpRequest();
+        oReq.addEventListener("load", () => {
+            const list = JSON.parse(oReq.responseText).body;
+            console.log(list);
+
+            list.forEach((value) => {
+                console.log(value.title);
+            })
+            
+
+
+        });
+
+        oReq.open('GET', dataURL);
+        oReq.send();
+
+
+
+    }
+    
 }
 
 
 export default Blog; 
+// npm run devserver 
